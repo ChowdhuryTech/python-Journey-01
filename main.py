4c329ccdd7e50325722b08a3901034a0ac78e453
ChowdhuryTech <chowdhurytech25@gmail.com>
2026-05-25 07:02:44 +0600

    age calculator

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello Umair!")
-print(1+2)
+# Build Age calculator.
+
+Birth_year = int(input("Enter your birth year: "))
+Current_Age = 2026 - Birth_year
+print("Your current age is: ", Current_Age)
+
