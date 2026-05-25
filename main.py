adf5e07315b7bb14cde7b099c543ac6c0405aa6e
ChowdhuryTech <chowdhurytech25@gmail.com>
2026-05-25 07:15:58 +0600

    basics

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ Birth_year = int(input("Enter your birth year: "))
 Current_Age = 2026 - Birth_year
 print("Your current age is: ", Current_Age)
 
+# Take input from the user and show them the output with modify message.
+Name = input("Enter your name: ")
+favorite_color = input("Enter your favorite color: ")
+print("You are " + str(Current_Age) + " years old.")
+
+print("Hello, " + Name + "! Your favorite color is " + favorite_color + ".")
